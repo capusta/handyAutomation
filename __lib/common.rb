a31f4bf6ba5888ae4f3abead4c9a988f959956aa
alexei <gvozdev@gmail.com>
2016-02-23 11:19:28 +0900

    Some basic cleanup

--- __lib/common.rb
+++ __lib/common.rb
@@ -1,3 +1,7 @@
+# Checks the directory and does some conversion if the directory
+# is Windows or Linux
+#
+#
 def checkDir(s)
   gracefulExit("#{s} is not a valid directory.") if !File.directory?(s || "")
   if s.include? "\\" then
@@ -9,21 +13,35 @@ def checkDir(s)
   s
 end
 
+# Same thing as checkDir ... except for files
+#
+#
+#
 def checkFile(s)
   gracefulExit("File #{s} does not exist") if !File.exist?(s || "")
   debugLog("File #{s} is OK")
   s
 end
 
+# Don't like uninformative error messages
+#
+#
+#
 def gracefulExit(msg)
   puts <<EOF
-  
+
   Error: #{msg}
   -- Exiting --
 EOF
   exit
 end
 
+# Some basic debugging
+#
+#
+#
 def debugLog(msg)
   puts "debug: #{msg}" if @options.debug
 end
+
+
