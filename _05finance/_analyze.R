c4a6fcb041323cafd2079f9d55d90919ee91f7a8
alexei <gvozdev@gmail.com>
2016-04-28 17:18:56 +0900

    more plotting for time-of-day breakdown

--- _05finance/_analyze.R
+++ _05finance/_analyze.R
@@ -10,13 +10,22 @@ library(gridExtra)
 
 # TODO: pass this variable from the script
 dateFormat <- "%Y-%m-%d-%H-%M"
+
+# The basic dataset
 mydata <- read.csv(fileName, header=FALSE, col.names=c("Date","Description","Amount","Category"))
-mydata$Date <- format(mydata$Date, format=dateFormat)
+mydata <- mydata[mydata$Amount > 0.0, ]
+mydata$Date <- as.POSIXct(mydata$Date, format=dateFormat)
+
+# Let's break it up into various parts for parsing
 mydata$Month.Name <- format(mydata$Date, format="%b")
 mydata$Month.Number <- format(mydata$Date, format="%m")
+mydata$hour <- as.numeric(format(mydata$Date, format="%H"))
+mydata$min <- format(mydata$Date, format="%M")
 
+# Cleanup of unnecessary field ... but maybe we can revert this if there is a need
+mydata <- mydata[, !(names(mydata) %in% c("Date"))]
 
-
+# Some basic plotting
 for (cat in levels(mydata$Category)){ 
   jpeg(paste0(reportsFolder,cat,".jpg"), width=600)
   
@@ -34,8 +43,13 @@ for (cat in levels(mydata$Category)){
     scale_fill_manual(values=topo.colors(12, alpha=.7)) +
     theme(legend.position='none', axis.text.x = element_text(angle=45), axis.title.x=element_blank())
   
+  counts = ggplot(mydata[mydata$Category == cat, ], aes(hour), xlim=c(1,20)) + 
+    facet_wrap(~Category) +
+    geom_histogram(binwidth = 0.5, aes(y = ..density..)) +
+    stat_function(fun = dnorm, color = 'red')
+  
 # g <- arrangeGrob(box, bar, ncol=1, nrow=2)
-  grid.arrange(total_sum, max_min, max_min, ncol=2, nrow=2 )
+  grid.arrange(total_sum, max_min, counts, ncol=2, nrow=2)
   
   #ggsave(paste0(cat,".jpg"), width = 4, height = 2, scale = 3.6)
   dev.off()
