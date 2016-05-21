7bfb3927292964c068c049372418ae66f951f695
alexei <gvozdev@gmail.com>
2016-05-22 00:40:11 +0900

    gave up on sorting by months, lets sort by day

--- 01convert.rb
+++ 01convert.rb
@@ -89,12 +89,12 @@ def do_video
 
     mov = MiniExiftool.new f
     movDate = mov["MediaCreateDate"] || mov.filecreatedate
-    mov_yr_mon_day = movDate.strftime("%Y.%m.%d") + " - "
+    mov_yr_mon_day = movDate.strftime("%Y.%m.%d")
     mov_yr_mon = movDate.strftime("%Y.%m")
     p "Exif Creation: #{mov_yr_mon_day}" if debug
     p "#{File.basename(f,".*")}", "#{myhash}" if debug
     mov_yr_mon_day = "" if @options.norename
-    outname = mov_yr_mon_day+File.basename(f,".*")+".mp4"
+    outname = mov_yr_mon_day+"-"+File.basename(f,".*")+".mp4"
 
     #quasi sorting of movies - handy for bulk
     nestedDir = @options.outputDir+mov_yr_mon_day
@@ -133,7 +133,7 @@ def do_pictures
   #no need to keep track of what we seen
   p 'starting photo mode' if @options.debug
 
-  approve = ["jpeg", ".jpg", "JPG","GIF","gif"]
+  approve = [".jpeg", ".jpg","gif"]
 
   Find.find(@options.inputDir){|f|
     debug = @options.debug
