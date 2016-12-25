83495ead9f8a906b754d30c4499d3ba1584527ad
capusta <gvozdev@gmail.com>
2016-12-24 17:28:00 -0800

    quick checks to make sure we make this idempotent

--- 07setup.ps1
+++ 07setup.ps1
@@ -1,8 +1,16 @@
 # Ensure set-execution policy is correctly set
 Set-ExecutionPolicy unrestricted -f
+$ch = choco --version
+if ($ch -eq $null) {
+    iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
 
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+$ch = git --version
+if ($ch -eq $null){
+    choco install git -y --force
+    }
 
-choco --version
-choco install git -y --force
-choco install  packer -y --force
+$ch = packer --version
+if ($ch -eq $null){
+    choco install  packer -y --force
+    }
