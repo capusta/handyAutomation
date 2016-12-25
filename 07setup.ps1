d58eb7d1073582bdc88160a682db28d8f4132ebd
capusta <gvozdev@gmail.com>
2016-12-24 17:12:48 -0800

    dont forget the execution policy

--- 07setup.ps1
+++ 07setup.ps1
@@ -1,4 +1,6 @@
 # Ensure set-execution policy is correctly set
+Set-ExecutionPolicy unrestricted -f
+
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 choco --version
