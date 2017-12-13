1e1380f35c5902afd015b96e15b6a4652916b7d3
Capusta <capusta@users.noreply.github.com>
2017-12-12 22:54:44 -0800

    Random stuff for variables

--- tensorflow/helloworld.py
+++ tensorflow/helloworld.py
@@ -16,3 +16,13 @@ sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
 out = sess.run(sum_op, feed_dict={b: x, c: y})
+print("done: {}".format(out))
+
+b = [ np.random.randint(low=1,high=10,size=5), np.random.randint(low=1,high=10,size=5)]
+print b
+
+rand_var_1 = tf.Variable(tf.random_uniform([5], 0, 10, dtype=tf.int32, seed=0))
+rand_var_1 = tf.random_uniform([5,3,2],0,10, dtype = tf.int32, seed = 0)
+print(sess.run(rand_var_1))
+
+rand_var_1 = tf.random_uniform([5],0,10, dtype = tf.int32, seed = 0)
