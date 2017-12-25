1c47878f5e62c109f03cc81dc27d917108b4cc4f
Capusta <capusta@users.noreply.github.com>
2017-12-25 01:36:07 -0800

    Meh ... random trainings for syntax

--- tensorflow/nn.py
+++ tensorflow/nn.py
@@ -45,13 +45,15 @@ saver = tf.train.Saver()
 
 with tf.Session() as sess:
   sess.run(init_op)
+  avg_cost = 0
   for epoch in range(epochs):
-    avg_cost = 0
     x_raw = [np.random.randint(low=1, high=60, size=20)]
     y_raw = [np.random.randint(low=0, high=2, size=5)]
     _, c = sess.run([optimiser, cross_entropy], feed_dict={x: x_raw, y: y_raw})
     # our cost is averaged over 10 epochs 
-    avg_cost += c / 10
-    print("Epoch:", (epoch + 1), "cost =", "{:.3f}".format(avg_cost))
     saver.save(sess, "./models/my_latest_model",global_step=1000)
 
+    avg_cost += c
+    print("Epoch:", (epoch + 1), "cost =", "{:.3f}".format(c))
+  avg_cos = avg_cost/10
+  print("--- Total run: {:.3f}".format(avg_cost))
