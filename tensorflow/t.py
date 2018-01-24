5b0f55f1464fce3f839a7b94f574a7106a8370c6
Capusta <capusta@users.noreply.github.com>
2018-01-24 00:33:05 -0800

    Dynamic doc length

--- tensorflow/t.py
+++ tensorflow/t.py
@@ -4,7 +4,6 @@ import tensorflow as tf
 from tensorflow.contrib import lookup
 from tensorflow.python.platform import gfile
 
-MAX_DOC_LENGTH = 5
 PADWORD = 'ZZZ'
 
 LINES = ['the quick brown fox',
@@ -12,19 +11,17 @@ LINES = ['the quick brown fox',
          'foo foo bar',
          'baz']
 
-
-vocab_processor = tf.contrib.learn.preprocessing.VocabularyProcessor(MAX_DOC_LENGTH)
-vocab_processor.fit_transform(LINES)
-
 init_op = tf.global_variables_initializer()
+MAX_DOC_LENGTH = max([len(L.split(" ")) for L in LINES])
+vocab_processor = tf.contrib.learn.preprocessing.VocabularyProcessor(MAX_DOC_LENGTH)
+vocab_processor.fit(LINES)
 
 with tf.Session() as sess:
     sess.run(init_op)
-    sess.run(vocab_processor)
 
     with gfile.Open('vocab.csv', 'wb') as f:
-        f.write("{}".format(PADWORD))
-        for w, i in vocab_processor.vocabulary_.mapping.iteritems():
-            f.write("{}".format(w))
+        f.write("{}\n".format(PADWORD))
+        for w, i in vocab_processor.vocabulary_._mapping.iteritems():
+            f.write("{}\n".format(w))
     NWORDS = len(vocab_processor.vocabulary_)
     print (NWORDS)
