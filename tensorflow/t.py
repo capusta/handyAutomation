92cbbcae7f4a25168af8227ba4e9c03f45252773
Capusta <capusta@users.noreply.github.com>
2018-01-27 00:39:58 -0800

    Vocab processor - loading and saving vocab file

--- tensorflow/t.py
+++ tensorflow/t.py
@@ -13,15 +13,28 @@ LINES = ['the quick brown fox',
 
 init_op = tf.global_variables_initializer()
 MAX_DOC_LENGTH = max([len(L.split(" ")) for L in LINES])
-vocab_processor = tf.contrib.learn.preprocessing.VocabularyProcessor(MAX_DOC_LENGTH)
-vocab_processor.fit(LINES)
 
-with tf.Session() as sess:
-    sess.run(init_op)
 
-    with gfile.Open('vocab.csv', 'wb') as f:
+def save_vocab(outfilename):
+    # the text to be classified
+    vocab_processor = tf.contrib.learn.preprocessing.VocabularyProcessor(MAX_DOC_LENGTH)
+    vocab_processor.fit(LINES)
+
+    with gfile.Open(outfilename, 'wb') as f:
         f.write("{}\n".format(PADWORD))
         for w, i in vocab_processor.vocabulary_._mapping.iteritems():
             f.write("{}\n".format(w))
     NWORDS = len(vocab_processor.vocabulary_)
-    print (NWORDS)
+    print('Vocab length: {}, File: {}'.format(NWORDS, outfilename))
+
+def load_vocab(infilename):
+    table = lookup.index_table_from_file(
+        vocabulary_file=infilename, num_oov_buckets=1, vocab_size=None, default_value=-1)
+    numbers = table.lookup(tf.constant('quick fox'.split()))
+    with tf.Session() as sess:
+        tf.tables_initializer().run()
+        print "{} --> {}".format(LINES[0], numbers.eval())
+
+
+save_vocab('model.tfmodel')
+load_vocab('model.tfmodel')
